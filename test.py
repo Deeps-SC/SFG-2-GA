3cb620e64e85e0cca7c785fa312b6ff740dc12f8
Deeps-SC <deep.shikha@slashcurate.com>
2025-07-29 17:27:43 +0530

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-with open('schedules.xml') as f1, open('producer.xml') as f2:
-    schedules_xml = f1.read()
-    producer_xml = f2.read()
-
-updated_producer_xml = inject_schedule_for_producer(schedules_xml, producer_xml)
-
-with open('updated_producer.xml', 'w') as fout:
-    fout.write(updated_producer_xml)
